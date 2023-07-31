acf3e1871fe68501be88598d611e6a4278db6882
tz-yu <F111143117@nkust.edu.tw>
2023-07-31 15:14:11 +0800

    edit function get max matchs for win number

--- src/invoice.py
+++ src/invoice.py
@@ -7,4 +7,16 @@ def get_last_n_digits(ticket, n):
 if __name__ == "_main_":
     ticket = '04667172'
     last_4_digits = get_last_n_digits(check, 4)
-    assert(last_4_digits) == '7172'S
+    assert(last_4_digits) == '7172'
+
+def get__max_maches_for_win_num(ticket,win_num):
+    matches = 0
+    for i in range(3,9):
+        last_n_digits = get_last_n_digits(ticket, i)
+        t = get_last_n_digits(ticket, i)
+        w = get_last_n_digits(win_num, i)
+        if t == w:
+            match_count = i
+        else:
+            break
+    return match_count
--- test_cases/test_invoice.py
+++ test_cases/test_invoice.py
@@ -1,5 +1,4 @@
-from src.invoice import get_last_n_digits
-
+from src.invoice import get_last_n_digits ,import get__max_maches_for_win_num
 def test_get_lastp_3_digits():
     ticket = '1234567890'
     last_3_digits = get_last_n_digits(ticket, 3)
@@ -9,3 +8,23 @@ def test_get_last_8_digits():
     ticket = '1234567890'
     last_8_digits = get_last_n_digits(ticket, 8)
     assert last_8_digits == '1234567890'
+
+
+
+def test_max_3_match():
+    ticket = '1234567890'
+    win_num = '1234567890'
+    max_matches = max_matches_for_win_num(ticket, win_num)
+    assert max_matches == 3
+
+def test_max_no_match():
+    ticket = '1234567890'
+    win_num = '1234567890'
+    max_matches = max_matches_for_win_num(ticket, win_num)
+    assert max_matches == 0
+
+def test_max_8_match():
+    ticket = '1234567890'
+    win_num = '1234567890'
+    max_matches = max_matches_for_win_num(ticket, win_num)
+    assert max_matches == 0
